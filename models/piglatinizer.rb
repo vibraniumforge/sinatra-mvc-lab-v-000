69724a999e49a36463133ddb6a0d2a8b0cf6650d
Greg Morgan <gregm9999@gmail.com>
2018-12-07 19:39:43 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,38 +2,39 @@ require "pry"
 class PigLatinizer
 
   def piglatinize(word)
-    new_word=word.split("")
-    alpha=("a".."z").to_a
-    alpha_uppercase=("A".."Z").to_a
-    vowels=%w[a e i o u A E I O U]
-    consonants= alpha_uppercase + alpha - vowels
-    result=''
-    if vowels.include?(new_word[0])
-      result="#{word}way"
-    elsif consonants.include?(new_word[0]) && consonants.include?(new_word[1])  && consonants.include?(new_word[2])
-      result="#{word[3..-1]}#{word[0..2]}ay"
-    elsif consonants.include?(new_word[0]) && consonants.include?(new_word[1])
-      result="#{word[2..-1]}#{word[0..1]}ay"
-    elsif consonants.include?(new_word[0])
-      result="#{word[1..-1]}#{word[0]}ay"
+    if word.include?(" ")
+      sentence_pig_latinizer(word)
     else
-      word
+      alpha=("a".."z").to_a
+      alpha_uppercase=("A".."Z").to_a
+      vowels=%w[a e i o u A E I O U]
+      consonants= alpha_uppercase + alpha - vowels
+      result=''
+      if vowels.include?(word[0])
+        result="#{word}way"
+      elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+        result="#{word[3..-1]}#{word[0..2]}ay"
+      elsif consonants.include?(word[0]) && consonants.include?(word[1])
+        result="#{word[2..-1]}#{word[0..1]}ay"
+      elsif consonants.include?(word[0])
+        result="#{word[1..-1]}#{word[0]}ay"
+      end
     end
+    # binding.pry
     result
   end
 
   def sentence_pig_latinizer(sentence)
-    words_of_a_sent=sentence.split(" ")
-    # binding.pry
-    pig_lat_words=words_of_a_sent.collect do |word|
+    words_of_a_sentence = sentence.split(" ")
+    pig_latinized_words = words_of_a_sentence.collect do |word|
       piglatinize(word)
     end
     # binding.pry
-    # pig_lat_sentence=pig_lat_words.join(" ")
-    #
-    # pig_lat_sentence
-    return pig_lat_words.join(" ")
-    binding.pry
+    final=pig_latinized_words.join(" ")
+    # binding.pry
+    final
   end
 
 end
+
+# PigLatinizer.new.sentence_pig_latinizer("I love programming")
